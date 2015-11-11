686164bb5ee6760e8739d925ee27e99d9c9b5ccc
Jack Schuss <jackschuss@gmail.com>
2015-11-10 20:14:41 -0500

    completed assignment 31

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,10 @@
 class Post < ActiveRecord::Base
   has_many :comments
+
+    @posts = Post.all
+    @posts.each_with_index do |post, index|
+    if index % 5 == 4 
+      post.update_attributes(title: 'SPAM')
+    end
+  end
 end
