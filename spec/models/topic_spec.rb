356c37894e9316e9b861cebe3c35e14ccf730798
Jack Schuss <jackschuss@gmail.com>
2015-11-16 20:35:26 -0500

    completed assignment 35

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -7,21 +7,6 @@ RSpec.describe Topic, type: :model do
 
   it {should have_many(:posts)}
 
-  describe "attributes" do
-    it "should respond to name" do
-      expect(topic).to respond_to(:name)
-    end
-
-    it "should respond to description" do
-      expect(topic).to respond_to(:description)
-    end
-
-    it "should respond to public" do
-      expect(topic).to respond_to(:public)
-    end
-
-    it "should be public by default" do
-      expect(topic.public).to be (true)
-    end
-  end
+  it { should validate_length_of(:name).is_at_least(5)}
+  it { should validate_length_of(:description).is_at_least(15)}
 end
