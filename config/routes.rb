0ff02690053b823828aa31ade43c10dbf370c087
Jack Schuss <jackschuss@gmail.com>
2015-11-22 11:26:31 -0500

    completed checkpoint 38

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
 
   resources :users, only: [:new, :create]
 
+  resources :sessions, only: [:new, :create, :destroy]
+
   post 'confirm' => 'users#confirm'
 
   get 'about' => 'welcome#about'
