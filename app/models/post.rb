f25c0070a1268ad650e92ded50612d128bf52bb0
Jack Schuss <jackschuss@gmail.com>
2015-11-23 12:51:49 -0500

    completed assignment 39

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,7 +4,10 @@ class Post < ActiveRecord::Base
   has_many :comments, dependent: :destroy
 
   default_scope { order('created_at DESC') }
-  
+
+  scope :ordered_by_title, -> { order(title: :asc) }
+  scope :ordered_by_reverse_created_at, -> { order('created_at ASC') }
+
   validates :title, length: {minimum: 5}, presence: true
   validates :body, length: {minimum: 20}, presence: true
   validates :topic, presence: true
