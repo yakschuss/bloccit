88c6a3873b52307f765bcbf67e7a7c2c6fa6414b
Jack Schuss <jackschuss@gmail.com>
2015-11-27 11:06:48 -0500

    refactor post_controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,8 @@
 class PostsController < ApplicationController
 
   before_action :require_sign_in, except: :show
-  before_action :authorize_user, except: [:show, :new, :create]
-
+  before_action :authorize_admin, only: [:destroy]
+  before_action :authorize_mod, only: [:edit, :update]
   def show
     @post = Post.find(params[:id])
   end
@@ -62,10 +62,18 @@ class PostsController < ApplicationController
     params.require(:post).permit(:title, :body)
   end
 
-  def authorize_user
+  def authorize_admin
+    post = Post.find(params[:id])
+      unless current_user == post.user || current_user.admin?
+        flash[:error] = "You must be an admin to do that."
+        redirect_to [post.topic, post]
+      end
+  end
+
+  def authorize_mod
     post = Post.find(params[:id])
       unless current_user == post.user || current_user.admin? || current_user.moderator?
-        flash[:error] = "You must be an admin to do that."
+        flash[:error] = "You need to be an admin or moderator to do that"
         redirect_to [post.topic, post]
       end
   end
