99eda8934036f2110665bb0386c41a5e1f68c5ad
Jack Schuss <jackschuss@gmail.com>
2015-12-01 12:45:25 -0500

    completed checkpoint 43

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151129003614) do
+ActiveRecord::Schema.define(version: 20151201124905) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -48,10 +48,19 @@ ActiveRecord::Schema.define(version: 20151129003614) do
     t.datetime "updated_at", null: false
     t.integer  "topic_id"
     t.integer  "user_id"
+    t.integer  "rating_id"
+    t.float    "rank"
   end
 
+  add_index "posts", ["rating_id"], name: "index_posts_on_rating_id"
   add_index "posts", ["topic_id"], name: "index_posts_on_topic_id"
   add_index "posts", ["user_id"], name: "index_posts_on_user_id"
+
+  create_table "ratings", force: :cascade do |t|
+    t.integer  "severity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "topics", force: :cascade do |t|
     t.string   "name"
@@ -59,7 +68,10 @@ ActiveRecord::Schema.define(version: 20151129003614) do
     t.text     "description"
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
+    t.integer  "rating_id"
   end
+
+  add_index "topics", ["rating_id"], name: "index_topics_on_rating_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -69,5 +81,16 @@ ActiveRecord::Schema.define(version: 20151129003614) do
     t.datetime "updated_at",      null: false
     t.integer  "role"
   end
+
+  create_table "votes", force: :cascade do |t|
+    t.integer  "value"
+    t.integer  "user_id"
+    t.integer  "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "votes", ["post_id"], name: "index_votes_on_post_id"
+  add_index "votes", ["user_id"], name: "index_votes_on_user_id"
 
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe Post, type: :model do
 
   it { should have_many(:labelings)}
   it { should have_many(:labels).through(:labelings)}
-  
+  it { should have_many (:votes)}
   it { should have_many (:comments)}
   it {should belong_to(:topic)}
   it {should belong_to(:user)}
@@ -21,4 +21,32 @@ RSpec.describe Post, type: :model do
   it { should validate_presence_of(:user)}
   it { should validate_length_of(:title).is_at_least(5)}
   it { should validate_length_of(:body).is_at_least(20)}
+
+
+  describe "voting" do
+    before do
+      3.times { post.votes.create!(value: 1) }
+      2.times { post.votes.create!(value: -1) }
+      @up_votes = post.votes.where(value: 1).count
+      @down_votes = post.votes.where(value: -1).count
+    end
+
+    describe "#up_votes" do
+      it "counts the number of votes with value = 1" do
+        expect( post.up_votes ).to eq(@up_votes)
+      end
+    end
+
+    describe "#down_votes" do
+      it "counts the number of votes with value = -1" do
+        expect( post.down_votes ).to eq(@down_votes)
+      end
+    end
+
+    describe "#points" do
+      it "returns the sum of all down and up votes" do
+        expect( post.points ).to eq(@up_votes - @down_votes)
+      end
+    end
+  end
 end
