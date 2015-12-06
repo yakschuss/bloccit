cc81fa8df63f07d7c0b9f2058cefe00e2824acab
Jack Schuss <jackschuss@gmail.com>
2015-12-06 15:58:38 -0500

    complete assignment 48

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,9 @@ class User < ActiveRecord::Base
   has_many :favorites, dependent: :destroy
   before_save { self.email = email.downcase}
   before_save { self.role ||= :member }
+
+  before_create :generate_auth_token
+
   EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
     validates :name, length: { minimum: 1, maximum: 100 }, presence: true
@@ -30,5 +33,12 @@ class User < ActiveRecord::Base
     def avatar_url(size)
       gravatar_id = Digest::MD5::hexdigest(self.email).downcase
       "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
+    end
+
+    def generate_auth_token
+      loop do
+        self.auth_token = SecureRandom.base64(64)
+        break unless User.find_by(auth_token: auth_token)
+      end
     end
 end
