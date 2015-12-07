5c9dcbf390e787e9fb824f5521f3a1f2e505e329
Jack Schuss <jackschuss@gmail.com>
2015-12-07 15:25:08 -0500

    complete 49, bloccit

--- app/controllers/api/v1/topics_controller.rb
+++ app/controllers/api/v1/topics_controller.rb
@@ -12,4 +12,41 @@ class Api::V1::TopicsController < Api::V1::BaseController
     topic = Topic.find(params[:id])
     render json: topic.to_json, status: 200
   end
+
+  def update
+    topic = Topic.find(params[:id])
+
+    if topic.update_attributes(topic_params)
+      render json: topic.to_json, status: 200
+    else
+      render json: {error: "Topic Update Failed", status: 400}, status: 400
+    end
+  end
+
+  def create
+    topic = Topic.new(topic_params)
+    if topic.valid?
+      topic.save!
+      render json: topic.to_json, status: 201
+    else
+      render json: {error: "Topic is invalid", status: 400}, status: 400
+    end
+  end
+
+  def destroy
+    topic = Topic.find(params[:id])
+
+    if topic.destroy
+      render json: { message: "Topic Destroyed", status: 200}, status: 200
+    else
+      render json: { error: "Topic Destroy Failed.", status: 400}, status: 400
+    end
+    
+  end
+
+  private
+
+  def topic_params
+    params.require(:topic).permit(:name, :description, :public)
+  end
 end
